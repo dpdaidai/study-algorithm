a6530758ae3596f72fd10a9d73a0739dba6c9780
cpt at mac <pantaochen@gmail.com>
2021-05-10 16:04:22 +0800

    测试二分查找

--- src/main/java/top/dpdaidai/algorithm/algorithms4th/chapter1/section1/sampleCode/BinarySearch.java
+++ src/main/java/top/dpdaidai/algorithm/algorithms4th/chapter1/section1/sampleCode/BinarySearch.java
@@ -1,5 +1,11 @@
 package top.dpdaidai.algorithm.algorithms4th.chapter1.section1.sampleCode;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
 /**
  * 二分查找作者提供了while循环实现和递归实现两种方式
  *
@@ -21,8 +27,8 @@ public class BinarySearch {
         if (end < start) return -1;
         //求当前区间中间数index
         int middle = start + (end - start) / 2;
-        if (key < array[middle]) return rankRecursion(key, array, start, middle);
-        else if (key > array[middle]) return rankRecursion(key, array, middle, end);
+        if (key < array[middle]) return rankRecursion(key, array, start, middle - 1);
+        else if (key > array[middle]) return rankRecursion(key, array, middle + 1, end);
         else return middle;
     }
 
@@ -41,5 +47,26 @@ public class BinarySearch {
         return -1;
     }
 
+    public static void main(String[] args) {
+
+        // java top.dpdaidai.algorithm.algorithms4th.chapter1.section1.sampleCode.BinarySearch tinyW.txt < tinyT.txt
+        // 一次读取整个args[0]的数据到whiteList中
+        int[] whiteList = In.readInts(args[0]);
+        Arrays.sort(whiteList);
+
+        int count = 0;
+
+        // tinyT.txt的数据则是由 StdIn.readInt() 输入流式挨个读取
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (rankRecursion(key, whiteList) < 0) {
+                StdOut.println(key);
+                count++;
+            }
+        }
+
+        StdOut.printf("Count : " + count);
+
+    }
 
 }
